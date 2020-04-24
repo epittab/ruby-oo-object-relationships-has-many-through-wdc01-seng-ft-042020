db5f2e8b4089869c03e95a531967dd94534eb65e
epittab <ep7m@me.com>
2020-04-23 21:22:05 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,37 @@
 class Customer
-  
+
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize (name, age)
+        @name, @age = name, age
+        @@all << self
+
+    end
+
+    #Class Methods
+
+    def self.all
+        @@all
+    end
+
+
+    #Instance Methods
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals()
+        Meal.all.select { |m| m.customer == self }
+    end
+
+    def waiters()
+        self.meals.map { |m| m.waiter }
+    end
+
+
+
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,34 @@
 class Waiter
 
+    attr_accessor :name, :years
+
+    @@all = []
+
+    def initialize(name, years)
+        @name, @years = name, years
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals()
+        Meal.all.select { |m| m.waiter == self }
+    end
+
+    def best_tipper()
+
+        largest_tip = [ self.meals[0].tip, self.meals[0].customer ]
+        self.meals.each { |t| largest_tip = [ t.tip, t.customer ] if t.tip > largest_tip[0]  }      
+        largest_tip[1]
+        
+    end
+
+
 end
